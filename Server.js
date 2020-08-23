6d6dfb09a77ec8ebaac2e58b69b941f7ccc95a41
Gustavo Lee <gglee89@gmail.com>
2020-08-23 08:04:38 -0400

    feat: Include 'website' folder with static content.
    Files copied from Udacity's starter project.

--- Server.js
+++ Server.js
@@ -1,19 +1,27 @@
-const bodyParser = require('body-parser');
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
-const PORT = 3000;
+const PORT = 8000;
+
+// Start up an instance of app
 const app = express();
 
-// Middleware
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors()); // For cross-origin allowance
 
-// Endpoints
-app.get('/', (req, res) => {
-    res.send({ text: 'Hello World' });
-});
+// Cors for cross origin allowance
+app.use(cors());
 
+// Initialize the main project folder
+app.use(express.static('website'));
+
+// Setup Server
 app.listen(PORT, () => {
-    console.log('Server started listening to port: ' + PORT);
+    console.log(`Localhost server started at port ${PORT}`);
 });
